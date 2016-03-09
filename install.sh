02eab506beacc2072bd05ddaa287dfc210ba673b
Ilya Portnov <portnov@iportnov.ru>
2016-03-09 20:46:51 +0500

    Update.

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ mkdir -p ~/.local/share/mime/packages/
 cp mimetypes/* ~/.local/share/mime/packages/
 sudo cp stripPicture /usr/local/bin/
 update-mime-database ~/.local/share/mime/
-kbuildsysoca5
+kbuildsycoca5
