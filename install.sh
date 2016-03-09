a0a4a3dc1ce60fc25db658753c938c0479112799
Ilya Portnov <portnov@iportnov.ru>
2016-03-09 20:31:30 +0500

    Add installation script.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ~/.local/share/kservices5/
+cp desktop/* ~/.local/share/kservices5/
+mkdir -p ~/.local/share/mime/packages/
+cp mimetypes/* ~/.local/share/mime/packages/
+sudo cp stripPicture /usr/local/bin/
