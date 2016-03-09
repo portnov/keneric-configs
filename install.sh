ff5e1aec07cb0fb03918f7076f12fa8b1928bccf
Ilya Portnov <portnov@iportnov.ru>
2016-03-09 20:32:10 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ cp desktop/* ~/.local/share/kservices5/
 mkdir -p ~/.local/share/mime/packages/
 cp mimetypes/* ~/.local/share/mime/packages/
 sudo cp stripPicture /usr/local/bin/
+update-mime-database ~/.local/share/mime/
+kbuildsysoca5
