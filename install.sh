76d3171706c7cccde634d023066b6eaa35c91223
Ilya Portnov <portnov@iportnov.ru>
2016-08-17 21:55:42 +0500

    Use image/x-gimp-palette for gimp palette files.
    it seems to be more standard.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/.local/share/kservices5/
 cp desktop/* ~/.local/share/kservices5/
 mkdir -p ~/.local/share/mime/packages/
-cp mimetypes/* ~/.local/share/mime/packages/
+#cp mimetypes/* ~/.local/share/mime/packages/
 sudo cp stripPicture /usr/local/bin/
-update-mime-database ~/.local/share/mime/
+#update-mime-database ~/.local/share/mime/
 kbuildsycoca5
